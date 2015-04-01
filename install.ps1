e599f9cd3a8bf67b67bb54860f0e28f12c1516cd
Craig Wilson <craiggwilson@gmail.com>
2015-04-01 17:09:35 -0500

    added in support for the MongoDB 3.0 log format.

--- install.ps1
+++ install.ps1
@@ -1,5 +1,3 @@
 cp BSON.opn $env:LOCALAPPDATA\Microsoft\MessageAnalyzer\OPNAndConfiguration\OPNs\Public\BSON.opn -Force
 cp MongoDB.opn $env:LOCALAPPDATA\Microsoft\MessageAnalyzer\OPNAndConfiguration\OPNs\Public\MongoDB.opn -Force
-
-#cmd /c mklink BSON.opn C:\Users\craig_000\AppData\Local\Microsoft\MessageAnalyzer\OPNAndConfiguration\OPNs\Public\BSON.opn
-#cmd /c mklink MongoDB.opn C:\Users\craig_000\AppData\Local\Microsoft\MessageAnalyzer\OPNAndConfiguration\OPNs\Public\BSON.opn
+cp MongoDBLog.config $env:LOCALAPPDATA\Microsoft\MessageAnalyzer\OPNAndConfiguration\TextLogConfiguration\DevicesAndLogs\MongoDBLog.config -Force
