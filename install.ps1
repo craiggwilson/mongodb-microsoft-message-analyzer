abab088983cd4666345d1f3744b4677f18848eb4
Craig Wilson <craiggwilson@gmail.com>
2015-08-12 17:13:03 -0500

    updated for analyzer 1.3.1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,2 @@
 cp BSON.opn $env:LOCALAPPDATA\Microsoft\MessageAnalyzer\OPNAndConfiguration\OPNs\Public\BSON.opn -Force
 cp MongoDB.opn $env:LOCALAPPDATA\Microsoft\MessageAnalyzer\OPNAndConfiguration\OPNs\Public\MongoDB.opn -Force
-cp MongoDBLog.config $env:LOCALAPPDATA\Microsoft\MessageAnalyzer\OPNAndConfiguration\TextLogConfiguration\DevicesAndLogs\MongoDBLog.config -Force
